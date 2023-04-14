6aa4c39fd0c5a38d00a8421b8195c3a4971a1226
Misha <bilanm20@gmail.com>
2023-04-14 16:57:25 +0300

    add constants and init game board.

--- init.cpp
+++ init.cpp
@@ -1,6 +1,24 @@
 ﻿#include <iostream>
 using namespace std;
 
+
+const int BOARD_S = 8;
+
+const char EMPTY = '-';
+const char O = 'O';
+const char X = 'X';
+
+char board[BOARD_S][BOARD_S] = {
+    { EMPTY, X, EMPTY, X, EMPTY, X, EMPTY, X },
+    { X, EMPTY, X, EMPTY, X, EMPTY, X, EMPTY },
+    { EMPTY, X, EMPTY, X, EMPTY, X, EMPTY, X },
+    { EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY },
+    { EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY },
+    { O, EMPTY, O, EMPTY, O, EMPTY, O, EMPTY },
+    { EMPTY, O, EMPTY, O, EMPTY, O, EMPTY, O },
+    { O, EMPTY, O, EMPTY, O, EMPTY, O, EMPTY }
+};
+
 int main()
 {
 	return 0;
